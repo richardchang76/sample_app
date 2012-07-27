92778eb36eaa0ec4c598d3b397ad7d2d2277564e
richard <richardchang76@gmail.com>
2012-07-27 15:30:24 -0700

    Chapter 8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,7 +1,7 @@
   require 'spec_helper'
 
 describe "UserPages" do
-subject { page }
+  subject { page }
 
   describe "signup page" do
     before { visit signup_path }
@@ -10,14 +10,14 @@ subject { page }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
 
-	describe "profile page" do
+  describe "profile page" do
 	  let(:user) { FactoryGirl.create(:user) }
 	  before { visit user_path(user) }
 	  it { should have_selector('h1',    text: user.name) }
 	  it { should have_selector('title', text: user.name) }
-	end
+  end
   
-   describe "signup" do
+  describe "signup" do
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
@@ -39,8 +39,14 @@ subject { page }
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
+          it { should have_selector('title', text: user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+		  it { should have_link('Sign out') }
+      end
     end
   end
-  
-  
 end
